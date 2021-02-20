ceb485b0960043a907690b755e9e8bc1b7a68433
Annie Tiet <anniedtiet@gmail.com>
2021-02-20 16:58:59 -0500

    help cmd example for aby

--- bot.py
+++ bot.py
@@ -16,11 +16,15 @@ async def event_message(ctx):
     print(ctx.content)
 
 @bot.event
-async def event_ready():
+async def bot_ready():
     'Called once when the bot goes online.'
     print(f"{os.environ['BOT_NICK']} is online!")
     ws = bot._ws  # this is only needed to send messages within event_ready
     await ws.send_privmsg(os.environ['CHANNEL'], f"/me has arrived! Is it getting hot in here?")
+
+@bot.command(name='help')
+async def help_command(ctx):
+    await ctx.send("Use !play command to start a game of hot bricktato!")
 
 if __name__ == "__main__":
     bot.run()
