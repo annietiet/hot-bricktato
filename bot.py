69d54d7b8a24dc8b786829aafedc0e950e1aed3e
aby <abigailtiet@gmail.com>
2021-02-20 22:21:07 -0500

    progress

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import os
 from twitchio.ext import commands
 import game
+from timer import Timer
 #pipenv run python bot.py
+
 
 bot = commands.Bot(
     irc_token=os.environ['TMI_TOKEN'],
@@ -12,6 +14,7 @@ bot = commands.Bot(
 )
 
 current_game = None
+boolean joinable = False
 
 @bot.event
 async def event_message(ctx):
@@ -30,21 +33,38 @@ async def bot_ready():
 async def help_command(ctx):
     await ctx.send("Use !play command to start a game of hot bricktato!")
 
+@bot.command(name='start')
+async def start_command(ctx):
+    global current game
+    if current_game is None:
+        current_game = game()
+        await ctx.channel.send(f"@{ctx.author.name} has started a hot bricktato game. Use !join within 20 seconds to start playing!")
+        t = Timer()
+        t.start()
+
+    else:
+        ctx.send("Wait until the current game is over")
+
+
 @bot.command(name='join')
 async def join_command(ctx):
     global current_game
     if current_game is None:
-        current_game = game()
-    game.add_player(current_game, ctx.author.name)
-    await ctx.channel.send(f"@{ctx.author.name} has joined the hot bricktato game")
+        ctx.send("No game currently going on. Use !play command to start a game of hot bricktato!")
+    elif joinable:
+        game.add_player(current_game, ctx.author.name)
+        await ctx.channel.send(f"@{ctx.author.name} has joined the hot bricktato game")
+    else:
+        ctx.send("Can't join at the moment")
 
 @bot.command(name='leave')
 async def leave_command(ctx):
     global current_game
     if current_game is None:
         ctx.send("No game currently going on. Use !play command to start a game of hot bricktato!")
-    game.add_player(current_game, ctx.author.name)
-    await ctx.channel.send(f"@{ctx.author.name} doesn't want to play...")
+    else:
+        game.remove_player(current_game, ctx.author.name)
+        await ctx.channel.send(f"@{ctx.author.name} doesn't want to play...")
 
 if __name__ == "__main__":
     bot.run()
--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-import bricktato
 
 class Game:
     players = set()
@@ -8,10 +7,7 @@ class Game:
 
     def add_player(self, username):
         self.players.add(username)
-        bricktato.update_num_players_label(len(self.players))
 
     def remove_player(self, username):
         if username in self.players:
             self.players.remove(username)
-            bricktato.update_num_players_label(len(self.players))
-
